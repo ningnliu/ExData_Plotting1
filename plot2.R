f9df8d5b9b0e67ff7a56fef90c333cdbe31b09f3
Ning Liu <30551659+ningnliu@users.noreply.github.com>
2017-08-25 11:23:24 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,9 +4,8 @@ data_use<-data[data$Date>="2007-02-01"&data$Date<="2007-02-02",] #extract the us
 data_use$Global_active_power <- as.numeric(data_use$Global_active_power) #change object type to numeric
 data_use<-cbind(strftime(paste(data_use[,1],data_use[,2]),format = "%Y-%m-%d %H:%M:%S"),data_use) #add the date and time
 names(data_use)[1]<-"Data_Time" #change the name of new column
-
-png(file="plot2.png",width = 480, height = 480,) #create the plot2.png
-with(data_use,plot(Data_Time,Global_active_power,ylab="Globel Active Powe (kilowatts)",xaxt="n")) #plot the data
-with(data_use,lines(Data_Time,Global_active_power)) #add line to the png
-axis(1,c(1,which(data_use$Data_Time=="2007-02-02 00:00:00"),length(data_use$Data_Time)),c("Thu","Fri","Sat")) #add the x-axis
-dev.off() #close the png file
+png(file="plot2.png",width = 480, height = 480,)
+with(data_use,plot(Data_Time,Global_active_power,ylab="Globel Active Powe (kilowatts)",type="l",xaxt="n"))
+with(data_use,lines(Data_Time,Global_active_power))
+axis(1,c(1,which(data_use$Data_Time=="2007-02-02 00:00:00"),length(data_use$Data_Time)),c("Thu","Fri","Sat"))
+dev.off()
